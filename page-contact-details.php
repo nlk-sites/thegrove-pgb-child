ddc7248e1b5bd1fb67d0579d2912940db8c5e15b
Akash Pagare <a.pagare@ninthlink.com>
2016-04-19 15:20:15 +0530

    GWC- 52 Changes as per comments and font changes

--- page-contact-details.php
+++ page-contact-details.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: Contact-pahrump-details
+ * Template Name: Contact-details
  *
  * @package pgb
  */
@@ -21,7 +21,28 @@ global $post;
             </div>
         </div>
     </div>
-
+	
+	<?php 
+	// Retriving the values of custom field of current page
+	
+	$address = '';
+	$city = '';
+	$number = '';
+	$timing = '';
+	$description = '';
+	$map_link = '';
+		
+	if ( function_exists( 'get_field' ) )
+	{
+		$address = get_field("address");
+		$city = get_field('city');
+		$number = get_field("number");
+		$timing = get_field("timing");
+		$description = get_field("description");
+		$map_link = get_field("map_link");
+	}
+	
+	?>
 
     <div class="theme-vertical-border"></div>
     <article class="main-content-area">
@@ -30,33 +51,31 @@ global $post;
                 <div class="col-md-9 left">
                     <div class="main-content-area-inner theme-bg" id="leftDiv">
 					
-						<div class="col-md-5">
+						<div class="col-md-6">
                             <div class="contact-map-blog">
                                 <h3><?php the_title(); ?></h3>
-                                <p><a target="_blank" href="http://maps.google.com/?q=<?php echo str_replace(" - Dispensing Soon", "", ot_get_option("pahrump_address")); ?> Pahrump"><?php echo ot_get_option("pahrump_address"); ?></a> <br> <?php echo ot_get_option("pahrump_number"); ?><br>
-                                 
-                                     <div class="time"><p><?php   echo ot_get_option("pahrump_timing"); ?></p></div>
-                                <span><?php echo get_teaser_text(ot_get_option("pahrump_description"),65); ?></span>
+								 <p><a target="_blank" href="http://maps.google.com/?q=<?php echo $address; echo $city; ?>"><?php echo $address; ?></a><br> <?php echo $number; ?>
+                                    <br>
+                                    <div class="time"><p><?php echo $timing; ?></p></div></p>
+                                <span><?php echo get_teaser_text($description,65); ?></span>
+							
                             </div>
                         </div>
                         <!-- End Col -->
-                        <div class="col-md-5 align-right map-view">
+                        <div class="col-md-6 align-right map-view">
                             <div class="contact-map-blog">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3218.8414987306414!2d-115.99062268487462!3d36.21904730789598!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c6377481109427%3A0x864b96292e25fb36!2s1541+E+Basin+Ave%2C+Pahrump%2C+NV+89060%2C+USA!5e0!3m2!1sen!2sin!4v1447333344460" width="100%" height="186" frameborder="0" style="border:0" allowfullscreen></iframe>
+                                <iframe src="<?php echo $map_link; ?>" width="100%" height="256" frameborder="0" style="border:0" allowfullscreen></iframe>
                             </div>
-							<a target="_blank" href="http://maps.google.com/?q=<?php echo str_replace(" - Dispensing Soon", "", ot_get_option("pahrump_address")); ?> Pahrump">Get Directions</a>
-							
+							<a target="_blank" href="http://maps.google.com/?q=<?php echo str_replace(" - Dispensing Soon", "", $address); echo $city;  ?> ">Get Directions</a>
                         </div>
 					<div class="clearfix"></div>
                         <!-- End Col -->
 					
-						<?php  echo do_shortcode('[espro-slider id=271]'); ?>
-						
+					
                         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                            <?php the_content(); ?>
+                          <?php the_content(); ?>
                         <?php endwhile; endif; ?>
-                        <!-- <h3>Contact</h3>
-                        <p>We are not just about providing medicine. We want to know how we can work together to extend the benefits of medical marijuana to more people, so we can create more wellness. Talk to us and let us know your thoughts.</p> -->
+                     
                         <div class="form-list contact-det-list">
                             <?php echo do_shortcode('[gravityform id="2" title="false" description="false" ajax="true"]'); ?>
                             <div class="clearfix"></div>
@@ -66,7 +85,6 @@ global $post;
                           <?php the_field('below_contact_form'); ?>
                         </div>
 
-                        
                     </div>
                 </div>
                 <!-- Aside starts -->
